dfb88c9f2d529b711cec65ee89a824170743df31
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-10-12 02:27:38 +0000

    Deploying to gh-pages from @ libscran/scran_markers@09bbeec658c9a15af71879e92273cbc5ecebac43 🚀

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['effect_20size_0',['Choice of effect size',['../namespacescran__markers.html#effect-sizes',1,'']]],
-  ['elementtype_1',['ElementType',['https://tatami-inc.github.io/tatami/namespacetatami.html#a83f5bea1c12d21f81732d6abd0665554',1,'tatami']]],
+  ['elementtype_1',['ElementType',['https://tatami-inc.github.io/tatami/namespacetatami.html#a7676f28b5618fd937e38b7a5c759d8bf',1,'tatami']]],
   ['elementtype_2ehpp_2',['ElementType.hpp',['https://tatami-inc.github.io/tatami/ElementType_8hpp.html',1,'']]],
-  ['end_3',['end',['https://tatami-inc.github.io/tatami/classtatami_1_1ArrayView.html#a904e6d03b59a20f2cc7831fbb318cf33',1,'tatami::ArrayView::end()'],['https://tatami-inc.github.io/tatami/classtatami_1_1SomeNumericArray.html#add771080790c65d2943c54a0615ee136',1,'tatami::SomeNumericArray::end()']]],
+  ['end_3',['end',['https://tatami-inc.github.io/tatami/classtatami_1_1ArrayView.html#a15127c10722fd3483010f38231dd7ee9',1,'tatami::ArrayView::end()'],['https://tatami-inc.github.io/tatami/classtatami_1_1SomeNumericArray.html#add771080790c65d2943c54a0615ee136',1,'tatami::SomeNumericArray::end()']]],
   ['extractor_2ehpp_4',['Extractor.hpp',['https://tatami-inc.github.io/tatami/Extractor_8hpp.html',1,'']]]
 ];
